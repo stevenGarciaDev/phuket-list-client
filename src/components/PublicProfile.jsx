9f2974a73f48944985815435eb87b21ba922977d
Richie E <repinosa@gmail.com>
2019-10-13 19:48:32 -0700

    fix private profile not displaying for owner

--- src/components/PublicProfile.jsx
+++ src/components/PublicProfile.jsx
@@ -15,6 +15,7 @@ import {
 import {
   getListItemsByID,
 } from "../services/bucketListService";
+import { getCurrentUser } from "../services/authService";
 
 class PublicProfile extends Component {
 	constructor(props) {
@@ -22,6 +23,7 @@ class PublicProfile extends Component {
 	    super(props);
 
 	    this.state = {
+	      reader_id: "",
 	      profile_id: this.props.match.params.user_id,
 	      profile_isActive: true,
 	      profile_isPrivate: false,
@@ -38,8 +40,19 @@ class PublicProfile extends Component {
 	}
 
 	async componentDidMount() {
+		try {
+			// get current user
+	    	const reader = getCurrentUser();
+	    	if (reader) {
+	    		this.setState({reader_id: reader._id});
+	    	}
+		} catch (e) {
+			console.log("error: ", e);
+		}
+
 		// Get JWT
 		const jwt = localStorage.getItem("token");
+
 		// Get user
 		const user = await getUserBasic(this.state.profile_id);
 
@@ -49,7 +62,7 @@ class PublicProfile extends Component {
 		// Get privacy status
 		this.setState({profile_isPrivate: user.data.isPrivateProfile});
 
-		if (!this.state.profile_isPrivate) {
+		if (!this.state.profile_isPrivate || (this.state.reader_id == this.state.profile_id) ) {
 			const userBio = await getUserBioByID(this.state.profile_id);
 		    this.setState({profile_bio: userBio.data[0].bio });
 		    const userPhoto = await getUserPhotoByID(this.state.profile_id);
@@ -71,10 +84,9 @@ class PublicProfile extends Component {
 		    const listItems = items.data[0].listItems;
 		    this.setState({ profile_listItems: listItems });
 		} else {
-			this.setState({profile_bio: "User account is private" });
+			//this.setState({profile_bio: "User account is private" });
 			this.setState({profile_avatar: "https://pbs.twimg.com/profile_images/901947348699545601/hqRMHITj_400x400.jpg" });
 		}
-		console.log(this.state.profile_avatar);
 	}
 
 	async loadPosts(jwt, id, limit, skip) {
@@ -116,47 +128,58 @@ class PublicProfile extends Component {
 						
 					</div>
 				</div>
-				<div className="row nopadding">
-					<div className="public-profile-bucket-list-module col-md-4">
-						<p className="module-title">{this.state.profile_name}'s Bucket List</p>
-						{this.state.profile_listItems.length > 0 &&
-		                    this.state.profile_listItems.map(item => (
-		                    <Link to={`/taskgroup/${item._id}`} key={item._id}>
-		                         <div className="activity-feed-user-groups-item">
-		                            <small>{item.taskName}</small>
-		                        </div>
-		                	</Link>
-		                ))}
-					</div>
-					<div className="public-profile-posts col-md-8">
-						{ this.state.profile_posts.length > 0 && this.state.profile_posts.map((post) => (
-				        	<Post
-				                key={post._id}
-				                id={post._id}
-				                author={post.author}
-				                image={post.image}
-				                dateCreated={post.dateCreated}
-				                text={post.text}
-				                likes={post.likes}
-				                comments={post.comments}
-				            />
-				           ))}
-						{ !this.state.feedEnd ?
-		                  (
-		                    <div className="activity-feed-body-load">
-		                      <small><i className="fa fa-level-down" aria-hidden="true"></i>Load More</small>
-		                      <BottomScrollListener onBottom={this.loadMorePosts.bind(this)} />
-		                    </div>
-		                  )
-		                  :
-		                  (
-		                    <div className="activity-feed-body-load">
-		                      <small>Nothing else to show... :(</small>
-		                    </div>
-		                  )
-		                }
-					</div>
-				</div>
+			{/* Only loads reset of page if profile is NOT private and READER != Profile Owner */}
+				{ (!this.state.profile_isPrivate || (this.state.reader_id == this.state.profile_id) ) ? 
+					(
+						<div className="row nopadding">
+							<div className="public-profile-bucket-list-module col-md-4">
+								<p className="module-title">{this.state.profile_name}'s Bucket List</p>
+								{this.state.profile_listItems.length > 0 &&
+				                    this.state.profile_listItems.map(item => (
+				                    <Link to={`/taskgroup/${item._id}`} key={item._id}>
+				                         <div className="activity-feed-user-groups-item">
+				                            <small>{item.taskName}</small>
+				                        </div>
+				                	</Link>
+				                ))}
+							</div>
+							<div className="public-profile-posts col-md-8">
+								{ this.state.profile_posts.length > 0 && this.state.profile_posts.map((post) => (
+						        	<Post
+						                key={post._id}
+						                id={post._id}
+						                author={post.author}
+						                image={post.image}
+						                dateCreated={post.dateCreated}
+						                text={post.text}
+						                likes={post.likes}
+						                comments={post.comments}
+						            />
+						           ))}
+								{ (!this.state.feedEnd) ?
+				                  (
+				                    <div className="activity-feed-body-load">
+				                      <small><i className="fa fa-level-down" aria-hidden="true"></i>Load More</small>
+				                      <BottomScrollListener onBottom={this.loadMorePosts.bind(this)} />
+				                    </div>
+				                  )
+				                  :
+				                  (
+				                    <div className="activity-feed-body-load">
+				                      <small>Nothing else to show... :(</small>
+				                    </div>
+				                  )
+				                }
+							</div>
+						</div>
+					)
+					:
+					(
+						<div className="activity-feed-body-load">
+							This user's profile is private.
+						</div>)
+
+				}
 			</div>
 		);
 	}
